9401af367750b8f0f02261b63e23fcffc164a78a
Ling Lu <ling.lu@hotmail.ca>
2020-08-19 22:35:55 -0400

    Initial Commit

---
+++ rock-paper-scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rock-paper-scissors
+//
+//  Created by Design Work on 2020-08-19.
+//  Copyright © 2020 Ling Lu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
